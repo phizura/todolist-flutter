22f5507473e87d9d0c863908ea4ca1b0c36a56a8
phizura <satrioapra@gmail.com>
2024-12-30 10:57:26 +0700

    feat(home-page): integrate provider for state management and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todolist_flutter/view_models/app_view_model.dart';
 import 'package:todolist_flutter/views/task_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(create: (context) => AppViewModel(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
