bc610e21180dbdbe9c9c0f4d373611b077afa844
phizura <satrioapra@gmail.com>
2024-12-30 10:58:03 +0700

    feat(home-page): add Task and User models with basic properties

---
+++ lib/view_models/app_view_model.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../models/task_model.dart';
+import '../models/user_model.dart';
+
+class AppViewModel extends ChangeNotifier {
+  List<Task> tasks = <Task>[];
+  User user = User("Cinderella");
+
+  Color clrLvl1 = Colors.grey.shade50;
+  Color clrLvl2 = Colors.grey.shade200;
+  Color clrLvl3 = Colors.grey.shade800;
+  Color clrLvl4 = Colors.grey.shade900;
+}
